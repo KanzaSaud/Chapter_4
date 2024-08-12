1fdf4e66261ddd2fa47746d37d90301b2d84e905
Kanza Saud <kanzasaud@gmail.com>
2024-08-12 06:39:04 +0500

    Done

---
+++ script.js
@@ -0,0 +1,11 @@
+var number, grade, school;  //Question 1
+//Question 2:
+//Legal variable names: 
+var time, dateOfBirth, _LocalLanguage, file1, $replay;
+//Illegal variablle names:
+//var 5jumps, your name, *flavour, inse~ct, marker!;
+document.write("<h1>"+"Rules for naming JS variables"+"</h1>");
+document.write("Variable names can only contain letters, digits, or the symbols $ and _. For example $my_1stVariable"+"<br>");
+document.write("Variables must begin with a letter, an underscore, or a dollar ($). For example $name, _name, or name"+"<br>");
+document.write("Variable names are case sensitive"+"<br>");
+document.write("Variable names should not be JS keywords");
